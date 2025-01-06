262b7cf0fe22835bbbf7d39c82814438aed5c1c8
melanielaporte <melaniemlaporte@protonmail.com>
2025-01-06 03:00:31 -0600

    Updating js.

--- styles.js
+++ styles.js
@@ -1,7 +1,7 @@
 
 // Wait for the DOM to load
 document.addEventListener("DOMContentLoaded", () => {
-    // Selectors
+    // Navigation Links - Selectors
     const navLinks = document.querySelectorAll("nav a");
   
     // Event Listener Example
